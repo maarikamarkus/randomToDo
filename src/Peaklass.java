3924b656106e0adf94fc93e77fa320affcb6ce95
maarikamarkus <maarika.markus@gmail.com>
2020-03-28 21:25:40 +0200

    peaklass

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Peaklass {
 
     public static void main(String[] args) {
         System.out.println("randomToDo");
-        String sisend = JOptionPane.showInputDialog("Sisesta uus tegevus: ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sisesta uus tegevus: ");
+        String sisend = scanner.nextLine();
+        System.out.println("Sisestasid: " + sisend);
     }
 }
