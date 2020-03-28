cd3a2c3d299c7012fd065498c81b7e496fbc031f
maarikamarkus <maarika.markus@gmail.com>
2020-03-28 21:19:15 +0200

    peaklass

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Peaklass {
+
     public static void main(String[] args) {
         System.out.println("randomToDo");
+        String sisend = JOptionPane.showInputDialog("Sisesta uus tegevus: ");
     }
 }
