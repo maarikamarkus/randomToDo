6f89d1f5d09fab2f48c2644a218156a44714c0c5
maarikamarkus <maarika.markus@gmail.com>
2020-03-28 21:29:01 +0200

    peaklass

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,10 +1,14 @@
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Peaklass {
 
     public static void main(String[] args) {
         System.out.println("randomToDo");
+        System.out.println("uus tegevus: -<sinu tegevus>");
+        System.out.println("välju tegevuste lisamisest: vajuta ENTER");
+        System.out.println("kuva to-do list: 1");
+        System.out.println("märgi tegevus tehtuks: +<sinu tegevus>");
+        System.out.println("kustuta tegevus: !<sinu tegevus>");
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Sisesta uus tegevus: ");
