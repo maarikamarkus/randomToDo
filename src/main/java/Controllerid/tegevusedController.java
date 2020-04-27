d40399040a944815dca3682cb946dc3bdf9776e8
maarikamarkus <maarika.markus@gmail.com>
2020-04-27 19:00:00 +0300

    tegevuste vaate põhi

---
+++ src/main/java/Controllerid/tegevusedController.java
@@ -0,0 +1,32 @@
+package Controllerid;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class tegevusedController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label tegevusedLisaLabel;
+
+    @FXML
+    private Label tegevusedMuudaLabel;
+
+    @FXML
+    private Label tegevusedSuvalineLabel;
+
+    @FXML
+    private Label tegevusedKustutaLabel;
+
+    @FXML
+    void initialize() {
+
+    }
+}
