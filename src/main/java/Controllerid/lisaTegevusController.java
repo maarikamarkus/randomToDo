ad4b1e327ab75e703871ff50459134c497597776
maarikamarkus <maarika.markus@gmail.com>
2020-04-27 20:21:15 +0300

    lisasin navigatsiooni tegevuse lisamise lehelt tegevuste listi lehele

--- src/main/java/Controllerid/lisaTegevusController.java
+++ src/main/java/Controllerid/lisaTegevusController.java
@@ -2,10 +2,15 @@ package Controllerid;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,11 +29,45 @@ public class lisaTegevusController {
     private Button lisaTegevusLisaNupp;
 
     @FXML
+    private Button lisaTegevusVaataTegevusiNupp;
+
+    @FXML
     void initialize() {
         lisaTegevusLisaNupp.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            Other.Tegevus uusTegevus = new Other.Tegevus(lisaTegevusUusTegevusField.getText(), false);
+            try {
+                liiguTegevusteVaatesse();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            //Other.Tegevus uusTegevus = new Other.Tegevus(lisaTegevusUusTegevusField.getText(), false);
 
         });
+
+        lisaTegevusVaataTegevusiNupp.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            try {
+                liiguTegevusteVaatesse();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+    }
+
+    private void liiguTegevusteVaatesse() throws IOException {
+        lisaTegevusLisaNupp.getScene().getWindow().hide();
+        FXMLLoader laadija = new FXMLLoader();
+        laadija.setLocation(getClass().getResource("/tegevused.fxml"));
+        //laadija.setLocation(getClass().getResource());
+
+        try {
+            laadija.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Parent root = laadija.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
     }
 
 
