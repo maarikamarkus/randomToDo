df626a01dcf9e3968037ed6d3f1df62ec7b642ea
maarikamarkus <maarika.markus@gmail.com>
2020-05-01 10:35:37 +0300

    navigatsiooni lisamine

--- src/main/java/Controllerid/kustutaTegevusController.java
+++ src/main/java/Controllerid/kustutaTegevusController.java
@@ -4,8 +4,15 @@ import Other.Tegevus;
 import Other.ToDoList;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class kustutaTegevusController {
@@ -15,12 +22,42 @@ public class kustutaTegevusController {
     @FXML
     ListView<String> kustutaTegevusList;
 
+
+    @FXML
+    private Button kustutaTegevusNäitaTegevusiNupp;
+
     @FXML
     void initialize() {
         for (Tegevus tegevus : tegevused) {
             kustutaTegevusList.getItems().add(tegevus.getKirjeldus());
         }
         kustutaTegevus();
+
+        kustutaTegevusNäitaTegevusiNupp.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                näitaTegevusi();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+    }
+
+    private void näitaTegevusi() throws IOException {
+        kustutaTegevusNäitaTegevusiNupp.getScene().getWindow().hide();
+        FXMLLoader laadija = new FXMLLoader();
+        laadija.setLocation(getClass().getResource("/tegevused.fxml"));
+
+        try {
+            laadija.load();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Parent root = laadija.getRoot();
+        Stage stage = new Stage();
+        stage.setTitle("randomToDo");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     public void kustutaTegevus() {
--- src/main/java/Other/SceneName.java
+++
@@ -1,6 +0,0 @@
-package Other;
-
-// inspiratsioon saadud https://github.com/ksnortum/javafx-multi-scene
-public enum  SceneName {
-    MAIN, LISATEGEVUS, TEGEVUSED, MUUDATEGEVUS, KUSTUTATEGEVUS, SUVALINETEGEVUS;
-}
