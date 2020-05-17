c8a984a7018e728f0307ad10127ae6d212c50083
maarikamarkus <maarika.markus@gmail.com>
2020-05-17 13:22:23 +0300

    tegin uue vaate, mis ühendab kõik eelmised (lisavaated) üheks

---
+++ src/main/java/Controllerid/toDoListController.java
@@ -0,0 +1,47 @@
+package Controllerid;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class toDoListController extends Controller{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button toDoListTehtudNupp;
+
+    @FXML
+    private Button toDoListMuudaNupp;
+
+    @FXML
+    private TextField toDoListMuudaTextfield;
+
+    @FXML
+    private Button toDoListKustutaNupp;
+
+    @FXML
+    private Button toDoListLisaUusNupp;
+
+    @FXML
+    private TextField toDoListUusTegevusTextfield;
+
+    @FXML
+    private Button toDoListLisaNupp;
+
+    @FXML
+    private Label toDoListRandomToDoLabel;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
