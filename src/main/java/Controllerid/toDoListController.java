d3085bbc086f6e71a9132f384ce21584df23a506
maarikamarkus <maarika.markus@gmail.com>
2020-05-19 12:41:23 +0300

    nüüd suurendamine on ka normaalsem

--- src/main/java/Controllerid/toDoListController.java
+++ src/main/java/Controllerid/toDoListController.java
@@ -7,13 +7,11 @@ import java.util.ResourceBundle;
 
 import Other.Tegevus;
 import Other.ToDoList;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class toDoListController extends Controller{
